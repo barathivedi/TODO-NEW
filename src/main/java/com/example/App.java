f7a131f7bbf7d353379b446adbc48c4919b75062
Barathi V <barathivedi@gmail.com>
2026-09-15 16:52:55 +0000

    Initial TODO application

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("TODO APPLICATION");
+        System.out.println("Application started successfully!");
+
+    }
+}
